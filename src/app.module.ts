c1dd67d10bdcbcf3e598e0307b2e14035354c6b6
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2024-10-03 09:02:40 -0300

    add .env file reader

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ProfessionalModule } from './professional/professional.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongodbModule } from './mongodb/mongodb.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ProfessionalModule, MongooseModule.forRoot('mongodb://localhost/nest'), MongodbModule, AuthModule],
+  imports: [ConfigModule.forRoot(), ProfessionalModule, MongooseModule.forRoot(process.env.MONGO_URI + process.env.MONGO_DB_NAME), MongodbModule, AuthModule],
   controllers: [],
   providers: [],
 })
