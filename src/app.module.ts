b887b13453aaddbae1670425d7f757e994f37d1d
Joao Lucas Ramos Suarez <ramossuarezjl@gmail.com>
2024-11-09 21:19:22 -0300

    migrate to Mongo >Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,15 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProfessionalModule, MongooseModule.forRoot(process.env.MONGO_URI + process.env.MONGO_DB_NAME), MongodbModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ProfessionalModule,
+    MongooseModule.forRoot(
+      process.env.MONGO_URI1 + process.env.MONGO_USER + ':' + process.env.MONGO_PASS + process.env.MONGO_HOST + process.env.MONGO_DB_NAME + process.env.MONGO_URI_OPTIONS,
+    ),
+    MongodbModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
